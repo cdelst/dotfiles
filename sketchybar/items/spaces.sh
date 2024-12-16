e2d993fbf36758a507c4b492add480fa7f7a9a97
Case Delst <cdelst@netflix.com>
2024-12-16 14:44:18 -0800

    Update configs

--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -4,7 +4,7 @@
 sketchybar --add event aerospace_workspace_change
 
 # Define your spaces with names, corresponding Nerd Font icons, and vibrant colors
-SPACES=("home::0xFFFF5733" "web:󰖟:0xFFFFFF33" "code::0xFF33FF57" "terminal::0xFF33FFFF" "slack:󰍩:0xFF5733FF" "music:󰎆:0xFFFF33A1" "notes:󱞁:0xFF8C33FF")
+SPACES=("home::0xFFFF5733" "web:󰖟:0xFFE69138" "code::0xFF33FF57" "terminal::0xFF33FFFF" "spotify:󰎆:0xFFFF33A1" "notes:󱞁:0xFF8C33FF")
 
 # Add and configure spaces
 for SPACE in "${SPACES[@]}"; do
